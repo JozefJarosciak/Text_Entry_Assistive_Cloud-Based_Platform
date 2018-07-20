c1e394a098555de3cd65cd716834a065aa0bd7e5
jarosciak@gmail.com <Gith1974>
2018-07-20 11:22:46 -0400

    continuous development

--- api/dictionary.php
+++ api/dictionary.php
@@ -1,8 +1,13 @@
 <?php
 $q = htmlspecialchars(mb_strtolower(($_GET["q"])));
+
+//$q = end(explode(PHP_EOL,$q));$q = end(explode('.',$q));
+
+$q = end(preg_split('/[^a-z0-9]/i', $q));
+
 include('connectdb.php');
 
-
+if (strlen($q)>=3) {
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -11,13 +16,17 @@ if ($conn->connect_error) {
 }
 
 //$sql = 'select * from textentry where word like "'.$q.'%" and word not like "'.$q.'"  order by LENGTH(word) ASC, rank ASC limit 10';
-$sql = 'select * from textentry where word like "'.$q.'%" and word not like "'.$q.'"  order by rank ASC limit 3';
+//$sql = 'select * from textentry where word like "'.$q.'%" and word not like "'.$q.'"  order by rank ASC limit 3';
+
+$sql = 'select * from en_english479k where word like "'.$q.'%" and word not like "'.$q.'" order by -rank DESC limit 10';
+
+
 $result = $conn->query($sql);
 $rows = array();
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        $rows[] = $row["word"];
+        $rows[] = strtolower ($row["word"]);
     }
 
 print json_encode($rows);
@@ -29,4 +38,5 @@ print json_encode($rows);
 
 
 $conn->close();
+}
 ?>
